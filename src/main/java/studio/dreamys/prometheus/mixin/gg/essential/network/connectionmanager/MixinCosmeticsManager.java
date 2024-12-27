7238395302bfc1a280293161ed28a5ac7987d2ac
dxxxxy <owner@dreamys.studio>
2024-12-27 15:21:46 -0500

    feat: revamp project

---
+++ src/main/java/studio/dreamys/prometheus/mixin/gg/essential/network/connectionmanager/MixinCosmeticsManager.java
@@ -0,0 +1,20 @@
+package studio.dreamys.prometheus.mixin.gg.essential.network.connectionmanager;
+
+import gg.essential.event.client.ClientTickEvent;
+import gg.essential.network.connectionmanager.cosmetics.CosmeticsManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = CosmeticsManager.class, remap = false)
+public abstract class MixinCosmeticsManager {
+    @Shadow
+    public abstract void unlockAllCosmetics();
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/CompletableFuture;complete(Ljava/lang/Object;)Z", shift = At.Shift.AFTER))
+    public void onTick(ClientTickEvent tickEvent, CallbackInfo ci) {
+        unlockAllCosmetics();
+    }
+}
